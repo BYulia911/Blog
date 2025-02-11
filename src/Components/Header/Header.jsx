4e0614fc5b5d5c41d9f6d013155f05b56fb2e279
BYulia911 <bakharevayulia34@gmail.com>
2025-02-11 22:30:51 +0300

    Добавление header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul className='nav'>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Categories</a></li>
+                    <li><a href='#'>About me</a></li>
+                </ul>
+        </nav>
+        </header>
+        
+    )
+}
+
+export default Header
