1559a2abfe79509f973caf68deda495ff237ca86
BYulia911 <bakharevayulia34@gmail.com>
2025-03-16 15:06:16 +0300

    Заполнение страницы About

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Footer from '/src/Components/Footer/Footer';
 import Home from '/src/Pages/Home/Home';
-import About from '/src/Pages/About';
+import About from '/src/Pages/About/About';
 
 function App() {
   return (
